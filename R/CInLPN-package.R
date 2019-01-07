9cc7fa45103c245322473ba7b5259d4a6516c0cc
bachirtadde <bachirtadde@yahoo.fr>
2019-01-07 11:14:38 +0100

    adding CInLPN-package. R file

---
+++ R/CInLPN-package.R
@@ -0,0 +1,21 @@
+#' A package for Causal Inference in a Latent Processes Network.
+#'
+#' This package combines Linear mixed model and difference equations features for dynamic modeling 
+#' of latent processes and their temporal influences with causal interpretation.
+#'
+#' \tabular{ll}{ Package: \tab CInLPN\cr Type: \tab Package\cr
+#' Version: \tab 0.2.0\cr Date: \tab 2019-01-03\cr License: \tab GPL (>= 2.0)\cr
+
+#'
+#' @name CInLPN-package
+#' @docType package
+#' @author Bachirou Tadd\'e, C\'ecile Proust-Lima
+#' Taddé O. B., et al. (2018). Dynamic modeling of multivariate latent processes 
+#' and their temporal relationships:  Application to Alzheimer's disease,  
+#' \underline{\it{in revision in Biometrics}}
+#' 
+#' @keywords Causality, Latent processes
+#' package
+#' 
+#' @useDynLib CInLPN
+NULL
