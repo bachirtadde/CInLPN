c876aa4e15a1a6ea45903ec586b224ff0d37fc4a
bachirtadde <bachirtadde@yahoo.fr>
2019-01-16 19:04:45 +0100

    Update CInLPN-package.R

--- R/CInLPN-package.R
+++ R/CInLPN-package.R
@@ -4,7 +4,7 @@
 #' of latent processes and their temporal influences with causal interpretation.
 #'
 #' \tabular{ll}{ Package: \tab CInLPN\cr Type: \tab Package\cr
-#' Version: \tab 0.2.0\cr Date: \tab 2019-01-03\cr License: \tab GPL (>= 2.0)\cr
+#' Version: \tab 0.2.0\cr Date: \tab 2019-01-03\cr License: \tab GPL (>= 2.0)\cr}
 
 #'
 #' @name CInLPN-package
@@ -12,7 +12,7 @@
 #' @author Bachirou Tadd\'e, C\'ecile Proust-Lima
 #' Taddé O. B., et al. (2018). Dynamic modeling of multivariate latent processes 
 #' and their temporal relationships:  Application to Alzheimer's disease,  
-#' \underline{\it{in revision in Biometrics}}
+#' in revision in Biometrics
 #' 
 #' @keywords Causality, Latent processes
 #' package
